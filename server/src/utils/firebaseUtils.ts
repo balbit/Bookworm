7939802484a9ef15aadc84b67ba44504a980da63
Elliot Liu <elliotliu123@gmail.com>
2024-05-11 18:05:01 -0400

    merge pdf on server

--- server/src/utils/firebaseUtils.ts
+++ server/src/utils/firebaseUtils.ts
@@ -1,15 +1,19 @@
 import { storage } from 'firebase-admin';
+import { PDFDocument } from 'pdf-lib';
 
 /**
  * Fetch pages from Firebase Storage
  */
-export async function getPagesFromFirebase(docId: string, pages: number[]): Promise<string[]> {
-    const promises = pages.map(page =>
-        fetchPageFromFirebase(`${docId}/page${page}.pdf`)
-    );
+export async function getPagesFromFirebase(docId: string, pages: number[]): Promise<string> {
+    const filePaths = pages.map(page => `${docId}/page${page}.pdf`);
+    const outputFileName = `${docId}/merged${pages}.pdf`;
+    return mergePdfs(filePaths, outputFileName);
+}
 
-    const filesData = await Promise.all(promises);
-    return filesData;
+export async function getChapterPagesFromFirebase(chapter: string): Promise<string> {
+    const filePaths = [`${docId}/${chapter}.pdf`];
+    const outputFileName = `${docId}/${chapter}.pdf`;
+    return mergePdfs(filePaths, outputFileName);
 }
 
 async function fetchPageFromFirebase(filePath: string): Promise<string> {
@@ -32,3 +36,30 @@ async function fetchPageFromFirebase(filePath: string): Promise<string> {
     const [url] = await file.getSignedUrl(signedUrlConfig);
     return url;
 }
+
+
+async function mergePdfs(filePaths: Array<string>, outputFileName: string): Promise<string> {
+    const bucket = storage().bucket();
+    const mergedPdfDoc = await PDFDocument.create();
+
+    for (const filePath of filePaths) {
+        const fileBuffer = (await bucket.file(filePath).download())[0];
+        const pdfDoc = await PDFDocument.load(fileBuffer);
+        const copiedPages = await mergedPdfDoc.copyPages(pdfDoc, pdfDoc.getPageIndices());
+        copiedPages.forEach(page => mergedPdfDoc.addPage(page));
+    }
+
+    const mergedPdfBytes = await mergedPdfDoc.save();
+    const outputFile = bucket.file(outputFileName);
+    const fileBuffer = Buffer.from(mergedPdfBytes);
+    await outputFile.save(fileBuffer, { contentType: 'application/pdf' });
+
+    const [url] = await outputFile.getSignedUrl({
+        version: 'v4',
+        action: 'read',
+        expires: Date.now() + 1000 * 60 * 60, // URL expires in 1 hour
+    });
+
+    return url;
+}
+
