d8306211e751659825de50c104db86da0ffb64d6
Elliot Liu <elliotliu123@gmail.com>
2024-05-11 10:45:12 -0400

    moved secrets

--- preprocessing/scripts/getCredentials.py
+++ preprocessing/scripts/getCredentials.py
@@ -1,7 +1,7 @@
 from firebase_admin import credentials, storage
 import firebase_admin
 
-cred = credentials.Certificate('../firebase_key.json')
+cred = credentials.Certificate('../../common/keys/firebase_key.json')
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'bookworm-85601.appspot.com'
 })
---
+++ server/env.sh
@@ -0,0 +1,1 @@
+export GOOGLE_APPLICATION_CREDENTIALS="../common/keys/firebase_key.json"
